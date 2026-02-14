1596bf7bd64731c21b4699cd50328dd3950b0521
Daniil <mr.dan1201@gmail.com>
2026-02-14 18:07:20 +0400

    Init homework lesson2

---
+++ Lesson2/dist/homework.js
@@ -0,0 +1,45 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+;
+const firstDev = {
+    level: 'junior',
+    login: 'qwerty123',
+    skills: ['JS', 'TS', 'HTML', 'CSS'],
+    isLookingForJob: true,
+};
+const secondDev = {
+    level: 'middle',
+    login: 'FghgACV12@!',
+    skills: ['JS', 'TS', 'Docker']
+};
+;
+;
+const calculateTotal = (cart) => {
+    let total = 0;
+    for (const item of cart) {
+        total += item.product.price * item.quantity;
+    }
+    return total;
+};
+const testCart1 = [
+    {
+        product: { id: 1, price: 100, title: 'Phone' },
+        quantity: 2
+    },
+    {
+        product: { id: 2, price: 345, title: 'PC' },
+        quantity: 1
+    },
+];
+const testCart2 = [
+    {
+        product: { id: 3, price: 1643, title: 'MacBook' },
+        quantity: 2
+    },
+    {
+        product: { id: 4, price: 1623, title: 'Car' },
+        quantity: 1
+    },
+];
+console.log(calculateTotal(testCart1));
+console.log(calculateTotal(testCart2));
--- Lesson2/dist/index.js
+++ Lesson2/dist/index.js
@@ -1,3 +1,15 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 // Lesson 2
+Object.defineProperty(exports, "__esModule", { value: true });
+const zombie = {
+    hp: 100,
+    attack: 20,
+    name: 'Zombie',
+    sword: 'diamond'
+};
+const skeleton = {
+    hp: 100,
+    attack: 20,
+    name: 'Zombie',
+    bow: 'emerald'
+};
